3168a78bd69459c24a7f33641cbf5e7a6dcc72fa
LonpatovaAdelina <91686774+LonpatovaAdelina@users.noreply.github.com>
2022-04-19 17:36:12 +0300

    Create cgi.h

---
+++ cgi.h
@@ -0,0 +1,8 @@
+#pragma once
+bool is_get();
+void get_from_data(char*& data);
+void get_param_value(char*& value, const char* param_name, const char* data);
+
+void str_decode(char*& dec_str, const char* enc_str);
+
+
